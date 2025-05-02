ed14c75039b779b214897d1888224b11c800dc1b
anandkhera <anandk3@nycstudents.net>
2025-05-02 00:13:42 +0000

    Done

---
+++ ReviewAnalysis.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+public class ReviewAnalysis {
+    private Review[] allReviews;
+
+    public ReviewAnalysis(Review[] reviews)
+    {
+        allReviews = reviews;
+    }
+
+    public double getAverageRating()
+    {
+        double sum = 0;
+        for(Review r : allReviews)
+        {
+            sum += r.getRating();
+        }
+        return sum / allReviews.length;
+    }
+        public ArrayList<String> collectComments() {
+            ArrayList<String> comments = new ArrayList<>();
+            for(int i = 0; i < allReviews.length; i++)
+            {
+                String s = i + "_" + allReviews[i].getComment();
+                String last = s.substring(s.length() - 1);
+                if(!last.equals(".") && !last.equals("!"))
+                {
+                    s += ".";
+                }
+                if((s.indexOf("!") > 0))
+            {
+                comments.add(s);
+            }
+        }
+        
+        return comments;
+        }
+        
+    }
